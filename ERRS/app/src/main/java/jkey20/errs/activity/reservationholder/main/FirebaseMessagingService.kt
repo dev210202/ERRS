7351cdd23b3ba4d32dbbe343eaa650efdc56329e
주이식 <helios2sic@gmail.com>
2022-05-10 22:24:44 +0900

    Feature: add foreground alarm

--- ERRS/app/src/main/java/jkey20/errs/activity/reservationholder/main/FirebaseMessagingService.kt
+++ ERRS/app/src/main/java/jkey20/errs/activity/reservationholder/main/FirebaseMessagingService.kt
@@ -1,22 +1,85 @@
 package jkey20.errs.activity.reservationholder.main
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import jkey20.errs.ERRSAplication
+import jkey20.errs.R
 
-class FirebaseMessagingService : FirebaseMessagingService() {
-//    override fun onMessageReceived(p0: RemoteMessage) {
-//        super.onMessageReceived(p0)
-//        Log.e("onMessageReceived", "!!")
-//    }
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+        //  포그라운드 실행
+        showNotification(p0)
+        Log.e("onMessageReceived", p0.toString())
+    }
+
+    override fun onMessageSent(p0: String) {
+        super.onMessageSent(p0)
+        Log.e("onMessageSent", p0)
+    }
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.e("onNewToken", p0)
+    }
+
+    private fun showNotification(remoteMessage: RemoteMessage){
+
+//        val notification = remoteMessage.notification
+//        Log.e("SHOW NOTIFICATION", " !!")
 //
-//    override fun onMessageSent(p0: String) {
-//        super.onMessageSent(p0)
-//        Log.e("onMessageSent", "!!")
-//    }
-//
-//    override fun onNewToken(p0: String) {
-//        super.onNewToken(p0)
-//        Log.e("onNewToken", "!!")
-//    }
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+//            PendingIntent.FLAG_ONE_SHOT) // FLAG_ONE_SHOT
+//        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val notificationBuilder = NotificationCompat.Builder(this)
+//            .setSmallIcon(R.mipmap.ic_launcher)
+//            .setContentTitle("title")
+//            .setContentText("body")
+//            .setAutoCancel(true)
+//            .setSound(soundUri)
+//            .setContentIntent(pendingIntent)
+//        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        notificationManager.notify(0, notificationBuilder.build())
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = "channel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.blue_gradient_background)
+            .setContentTitle("title")
+            .setContentText("body")
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+
+    }
+
 }
