3f0b72ef6ffc20612bdc0837399b67f22f1bb050
주이식 <helios2sic@gmail.com>
2022-05-15 00:16:40 +0900

    Chore: remove serverrepository DI

--- ERRS/app/src/main/java/jkey20/errs/di/RepositoryModule.kt
+++ ERRS/app/src/main/java/jkey20/errs/di/RepositoryModule.kt
@@ -6,7 +6,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 import jkey20.errs.repository.FirebaseRepository
-import jkey20.errs.repository.ServerRepository
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -14,8 +13,4 @@ object RepositoryModule {
     @Provides
     @ViewModelScoped
     fun provideFirebaseRepository() : FirebaseRepository = FirebaseRepository()
-
-    @Provides
-    @ViewModelScoped
-    fun provideServerRepository() : ServerRepository = ServerRepository()
 }
