ae6ea33690524554adcad45ab306d69968f1f181
주이식 <helios2sic@gmail.com>
2022-05-15 13:31:18 +0900

    Chore: refactor menu property

--- ERRS/app/src/main/java/jkey20/errs/model/firebase/Menu.kt
+++ ERRS/app/src/main/java/jkey20/errs/model/firebase/Menu.kt
@@ -7,7 +7,6 @@ import java.io.Serializable
 data class Menu(
     val name: String = "",
     val price: Int = 0,
-    val pay: String = "",
     val status: String = "",
     val info: String = "",
     val uri: String = ""
